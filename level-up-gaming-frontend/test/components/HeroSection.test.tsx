1f2180d255fa28c0947a1733cdf966cfa9f1456c
LuBeneventi <lu.beneventi@duocuc.cl>
2025-11-18 20:57:28 -0300

    Cambios 18-11

--- level-up-gaming-frontend/test/components/HeroSection.test.tsx
+++ level-up-gaming-frontend/test/components/HeroSection.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import HeroSection from '../../src/components/HeroSection';
+import { describe, expect, it } from 'vitest';
 
 describe('HeroSection', () => {
   it('should render correctly', () => {
