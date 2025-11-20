e0b4f732864ae8424092805e9b24f7733d899d58
LuBeneventi <lu.beneventi@duocuc.cl>
2025-11-19 23:55:45 -0300

    19-11

--- level-up-gaming-frontend/test/pages/AboutUsPage.test.tsx
+++ level-up-gaming-frontend/test/pages/AboutUsPage.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import AboutUsPage from '../../src/pages/AboutUsPage';
+import { describe, expect, it } from 'vitest';
 
 describe('AboutUsPage', () => {
   it('should render correctly', () => {
